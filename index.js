5cad24d51da53993ec67dec937b85c583181f321
carmelolg <carmelolagamba@hotmail.it>
2017-12-12 16:23:09 +0100

    refs #master - second init

--- index.js
+++ index.js
@@ -3,22 +3,17 @@ const Parameters = require('./credentials.js')
 
 const Telegram = require('telegram-node-bot')
 const TextCommand = Telegram.TextCommand
-const RegexpCommand = Telegram.RegexpCommand
 const telegram = new Telegram.Telegram(Parameters.token, {
     workers: 1
 })
 
+const PharmacyController = require("./pharmacy").PharmacyController;
 const StartController = require("./start").StartController;
 const OtherwiseController = require("./otherwise").OtherwiseController;
-const PostsController = require("./posts").PostsController;
-const PagesController = require("./pages").PagesController;
-const AuthorController = require("./author").AuthorController;
+
 
 telegram.router
-    .when(new RegexpCommand(/ciao|hey|we|ping/i), new StartController())
     .when(new TextCommand('/start'), new StartController())
-    .when(new TextCommand('/last'), new PostsController())
-    .when(new TextCommand('/pages'), new PagesController())
-    .when(new TextCommand('/author'), new AuthorController())
+    .when(new TextCommand('/somethings'), new PharmacyController())
     .otherwise(new OtherwiseController())
 
--- pharmacy/index.js
+++ pharmacy/index.js
@@ -1,8 +1,24 @@
+const Telegram = require('telegram-node-bot')
+const TelegramBaseController = Telegram.TelegramBaseController
 
-// var request = require('request');
-// request("http://opendatasalutedata.cloudapp.net/v1/datacatalog/AziendeSanitarieLocali/?$filter=denominazioneazienda eq 'RM/A' &format=json",
-//     function (error, response, body) {
-//         if (!error && response.statusCode == 200) {
-//             console.log(body) // Print the google web page.
-//         }
-//     })
+class PharmacyController extends TelegramBaseController {
+
+    /**
+     * @param {Scope} scope
+     */
+    handle(scope) {
+        scope.sendMessage('Work in progress')
+
+
+        // var request = require('request');
+        // request("http://opendatasalutedata.cloudapp.net/v1/datacatalog/AziendeSanitarieLocali/?$filter=denominazioneazienda eq 'RM/A' &format=json",
+        //     function (error, response, body) {
+        //         if (!error && response.statusCode == 200) {
+        //             console.log(body) // Print the google web page.
+        //         }
+        //     })
+    }
+}
+
+
+module.exports.PharmacyController = PharmacyController;
